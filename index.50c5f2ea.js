db4f34a91d70f6b2f9c28ae9bd96a033689fd6f3
Oleksandr <tokyopac@gmail.com>
2024-11-29 22:21:12 +0200

    make build

---
+++ index.50c5f2ea.js
@@ -0,0 +1,2 @@
+var e=document.getElementById("largeImg");document.getElementById("thumbs").onclick=function(t){var l=t.target.closest("a");l&&(e.src=l.href,e.alt=l.title,t.preventDefault())};
+//# sourceMappingURL=index.50c5f2ea.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,12 @@
+"use strict";
+const mainImage = document.getElementById("largeImg");
+const thumbs = document.getElementById("thumbs");
+thumbs.onclick = function(e) {
+    const thumbnail = e.target.closest("a");
+    if (!thumbnail) return;
+    mainImage.src = thumbnail.href;
+    mainImage.alt = thumbnail.title;
+    e.preventDefault();
+};
+
+//# sourceMappingURL=index.f75de5e1.js.map
